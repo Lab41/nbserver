8c177eec8edd0006fd9a86ce7b9b91a28c536971
kylemvz <kylez@lab41.org>
2016-12-16 18:41:06 +0000

    Change server_extensions to nbserver_extensions since server_extensions is deprecated.

--- 02_ganymede/ganymede/jupyter_notebook_config.py
+++ 02_ganymede/ganymede/jupyter_notebook_config.py
@@ -1,7 +1,7 @@
-c.NotebookApp.server_extensions = [
-    'ganymede.ganymede',
-    'jupyter_nbgallery'
-]
+c.NotebookApp.nbserver_extensions = {
+    'ganymede.ganymede': 'ganymede.ganymede',
+    'jupyter_nbgallery': 'jupyter_nbgallery'
+}
 c.NotebookApp.allow_origin = 'https://nb.gallery'
 
 from ganymede.ganymede import GanymedeHandler
